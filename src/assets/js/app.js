80d3eb7f86102bb5e4c90f0defd2e3e9e76034c4
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-10-14 19:13:59 +0200

    Linked styles for app & Added basic opening/closing mechanic of the widget

---
+++ src/assets/js/app.js
@@ -0,0 +1,25 @@
+//* SCSS
+import '../sass/app.scss';
+
+class Chat {
+    constructor() {
+        
+    }
+    setElements() {
+        this.elements = {};
+        this.elements.chatHeader = document.querySelector('.js-chat__header');
+        this.elements.chatWindow = document.querySelector('.js-chat__content');
+    }
+    setUpEvents() {
+        this.elements.chatHeader.addEventListener('click', () => {
+            this.elements.chatWindow.classList.toggle('chat__content--opened');
+        })
+    }
+    init() {
+        this.setElements();
+        this.setUpEvents();
+    }
+}
+
+const chat = new Chat();
+chat.init();
