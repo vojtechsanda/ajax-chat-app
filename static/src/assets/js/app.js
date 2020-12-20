129808ad037874139c79c3e2fae04897f8fdd842
Vojtěch Šanda <vojtech.sanda@gmail.com>
2020-12-20 16:13:29 +0100

    Added basic view switching logic

--- static/src/assets/js/app.js
+++ static/src/assets/js/app.js
@@ -3,11 +3,46 @@ import '../sass/main.scss';
 
 import Registration from './components/registration'
 import UserAuthentication from './components/UserAuthentication'
+import Chat from './components/Chat'
+import Redirector from './components/Redirector'
 
-const registration = new Registration;
-const userAuthentication = new UserAuthentication;
+class App {
+    constructor() {
+        this.state = {};
+        this.views = {};
+        this.utilities = {};
+    }
+    analyzeUser() {
+        this.state.token = localStorage.getItem('token');
+    }
+    showView(view = 'auto') {
+        if (view === 'auto') {
+            const currentUrlSearch = location.search.slice(1);
 
-registration.init();
-userAuthentication.init();
+            if (this.state.token && currentUrlSearch !== 'chat') {
+                this.utilities.redirector.toChat();
+            } else if (!this.state.token && (currentUrlSearch !== 'login' && currentUrlSearch !== 'registration')) {
+                this.utilities.redirector.toLogin();
+            }
 
-console.log(registration);
+            this.views[currentUrlSearch].init(true);
+        } else {
+            this.utilities.redirector.changeSearch(view);
+        }
+    }
+    setupViews() {
+        this.views.registration = new Registration;
+        this.views.login = new UserAuthentication;
+        this.views.chat = new Chat;
+    }
+    init() {
+        this.utilities.redirector = new Redirector;
+
+        this.setupViews();
+        this.analyzeUser();
+        this.showView();
+    }
+}
+
+const app = new App;
+app.init();
